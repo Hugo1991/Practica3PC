59b967724d767b472894b62523156202ea8d2b57
Hugo1991 <hugofernandezvisier@gmail.com>
2016-05-15 14:11:17 +0200

    Filtro modificado, me lo hace perfecto

--- src/Filtro.java
+++ src/Filtro.java
@@ -1,60 +1,57 @@
-public class Filtro{
+public class Filtro {
 	private final static int[][] matriz = new int[3][3];
 
-	public Filtro(String matrizEntera){
+	public Filtro(String matrizEntera) {
 		crearFiltro(matrizEntera);
 	}
 
-	private static void crearFiltro(String matrizEntera){
+	private static void crearFiltro(String matrizEntera) {
 		String[] fila = matrizEntera.split(",");
-		for(int i = 0;i < matriz.length;i++){
+		for (int i = 0; i < matriz.length; i++) {
 			String[] numero = fila[i].split("\\s+");
-			for(int j = 0;j < matriz.length;j++){
+			for (int j = 0; j < matriz.length; j++) {
 				matriz[i][j] = Integer.parseInt(numero[j]);
 			}
 		}
 	}
 
-	public static void mostrarFiltro(){
-		for(int i = 0;i < matriz.length;i++){
-			for(int j = 0;j < matriz.length;j++){
+	public static void mostrarFiltro() {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
 				System.out.print(matriz[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
 
-	public static void aplicarFiltroPixel(int[][] imagen,int x,int y, int [][]imagenDestino){
+	public static void aplicarFiltroPixel(int[][] imagen, int x, int y, int[][] imagenDestino) {
 		int suma = 0;
-		int posFiltroX=0;
-		for(int fila = x - 1;fila <= x + 1;fila++){
-			int posFiltroY=0;
-			for(int col = y - 1;col <= y + 1;col++){
+		int posFiltroX = 0;
+		for (int fila = x - 1; fila <= x + 1; fila++) {
+			int posFiltroY = 0;
+			for (int col = y - 1; col <= y + 1; col++) {
 				suma += (imagen[fila][col] * matriz[posFiltroX][posFiltroY]);
 				posFiltroY++;
 			}
 			posFiltroX++;
 		}
-		suma=Math.abs(suma);
-		if(suma > 255){
-			suma /= getValorAbsoluto();
-		}
-		imagenDestino[x][y]=suma;
+		suma /= getValorAbsoluto();
+		imagenDestino[x][y] = Math.abs(suma);
 	}
 
-	private static int getValorAbsoluto(){
+	private static int getValorAbsoluto() {
 		int sumaPos = 0;
 		int sumaNeg = 0;
-		for(int i = 0;i < matriz.length;i++){
-			for(int j = 0;j < matriz.length;j++){
-				if(matriz[i][j] < 0){
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				if (matriz[i][j] < 0)
 					sumaNeg += matriz[i][j];
-				}else{
+				else
 					sumaPos += matriz[i][j];
-				}
+
 			}
 		}
-		sumaNeg=Math.abs(sumaNeg);
-		return Math.max(sumaPos,sumaNeg);
+		sumaNeg = Math.abs(sumaNeg);
+		return Math.max(sumaPos, sumaNeg);
 	}
 }
